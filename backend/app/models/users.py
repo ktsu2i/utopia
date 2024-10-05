13ab7d91ad499ca32d1a7d4f4282403c195fb9ec
Kaito Tsutsui <kaito.tsu2i@gmail.com>
2024-10-06 03:50:36 +0900

    removed all in backend

--- backend/app/models/users.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from database import Base
-
-class User(Base):
-  __tablename__ = "users"
-
-  id = Column(Integer, primary_key=True)
-  full_name = Column(String(50), index=True)
-  email = Column(String(50), unique=True, index=True)
