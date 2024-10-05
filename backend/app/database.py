f4e80f2ed9b5e3b60cf17965d97cb8f24eb48693
Kaito Tsutsui <kaito.tsu2i@gmail.com>
2024-10-06 03:13:20 +0900

    re-added mysqlclient

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,11 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import pymysql
+# import pymysql
 
-DATABASE_URL = "mysql+pymysql://admin:admin@127.0.0.1:3306/utopia_dev"
+DATABASE_URL = "mysql://admin:admin@localhost:3306/utopia_dev"
 
-Engine = create_engine(DATABASE_URL)
+Engine = create_engine(DATABASE_URL, encoding="utf-8", echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=Engine)
 
 Base = declarative_base()
+
+def get_db():
+  db = SessionLocal()
+  try:
+    yield db
+  finally:
+    db.close()
--- backend/app/models/users.py
+++ backend/app/models/users.py
@@ -5,4 +5,5 @@ class User(Base):
   __tablename__ = "users"
 
   id = Column(Integer, primary_key=True)
-  name = Column(String)
+  full_name = Column(String(50), index=True)
+  email = Column(String(50), unique=True, index=True)
