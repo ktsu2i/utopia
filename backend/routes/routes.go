77ca62f1f0dac2d3f9521fea02edc52e2efd380c
Kaito Tsutsui <kaito.tsu2i@gmail.com>
2024-10-06 17:36:33 +0900

    changed api route

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -3,6 +3,6 @@ package routes
 import "github.com/labstack/echo/v4"
 
 func RegisterRoutes(e *echo.Echo) {
-	e.GET("/user", getAllUsers)
+	e.GET("/users", getAllUsers)
 	e.GET("/user/:id", getUserById)
 }
