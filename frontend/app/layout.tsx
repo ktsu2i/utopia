559ff31716483bc96d8604b341b54b59a6b8959e
Kaito Tsutsui <101069375+ktsu2i@users.noreply.github.com>
2024-10-12 15:22:05 +0900

    Created sign up page (#4)

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -19,8 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={poppins.className}>
+    <html lang="en" className="h-full">
+      <body className={`h-full ${poppins.className}`}>
         {children}
       </body>
     </html>
