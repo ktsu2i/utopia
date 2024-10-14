47f046506acdc9a359cf00fb212835a05fcc0a3c
Kaito Tsutsui <101069375+ktsu2i@users.noreply.github.com>
2024-10-14 15:45:15 +0900

    Implement refresh token (#16)

--- backend/handlers/auth.go
+++ backend/handlers/auth.go
@@ -22,8 +22,8 @@ func hash(password string) (string, error) {
 	return string(hashed), nil
 }
 
-func ValidateToken(c echo.Context) error {
-	cookie, err := c.Cookie("token")
+func refreshToken(c echo.Context) error {
+	cookie, err := c.Cookie("refresh_token")
 	if err != nil {
 		return c.JSON(http.StatusUnauthorized, map[string]string{"message": "Unauthorized"})
 	}
@@ -41,6 +41,55 @@ func ValidateToken(c echo.Context) error {
 	claims, ok := token.Claims.(*models.AccountClaims)
 	if !ok || !token.Valid {
 		return c.JSON(http.StatusUnauthorized, map[string]string{"message": "Unauthorized"})
+	}
+
+	newClaims := &models.AccountClaims{
+		ID: claims.ID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour * 1)),
+		},
+	}
+
+	// Create new JWT
+	token = jwt.NewWithClaims(jwt.SigningMethodHS256, newClaims)
+	t, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
+	}
+
+	// Set new JWT to HTTP-Only cookie
+	cookie = &http.Cookie{
+		Name:     "token",
+		Value:    t,
+		HttpOnly: true,
+		Secure:   c.Scheme() == "https",
+		Path:     "/",
+		SameSite: http.SameSiteLaxMode,
+	}
+	c.SetCookie(cookie)
+
+	return c.JSON(http.StatusOK, map[string]string{"id": newClaims.ID})
+}
+
+func ValidateToken(c echo.Context) error {
+	cookie, err := c.Cookie("token")
+	if err != nil {
+		return refreshToken(c)
+	}
+
+	token, err := jwt.ParseWithClaims(cookie.Value, &models.AccountClaims{}, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(os.Getenv("JWT_SECRET")), nil
+	})
+	if err != nil || !token.Valid {
+		return refreshToken(c)
+	}
+
+	claims, ok := token.Claims.(*models.AccountClaims)
+	if !ok {
+		return refreshToken(c)
 	}
 
 	return c.JSON(http.StatusOK, map[string]string{"id": claims.ID})
@@ -91,7 +140,7 @@ func SignUp(c echo.Context) error {
 		Secure:   c.Scheme() == "https",
 		Path:     "/",
 		Domain:   "localhost",
-		SameSite: http.SameSiteStrictMode,
+		SameSite: http.SameSiteLaxMode,
 	}
 	c.SetCookie(cookie)
 
@@ -138,6 +187,30 @@ func Login(c echo.Context) error {
 	}
 	c.SetCookie(cookie)
 
+	// Create refresh token
+	refreshClaims := &models.AccountClaims{
+		ID: u.ID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour * 24 * 7)),
+		},
+	}
+	newToken := jwt.NewWithClaims(jwt.SigningMethodHS256, refreshClaims)
+	rt, err := newToken.SignedString([]byte(os.Getenv("JWT_SECRET")))
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
+	}
+
+	// Set refresh token to HTTP-Only Cookie
+	refreshCookie := &http.Cookie{
+		Name:     "refresh_token",
+		Value:    rt,
+		HttpOnly: true,
+		Secure:   c.Scheme() == "https",
+		Path:     "/",
+		SameSite: http.SameSiteLaxMode,
+	}
+	c.SetCookie(refreshCookie)
+
 	return c.JSON(http.StatusOK, map[string]string{"message": "successfully logged in"})
 }
 
@@ -152,5 +225,17 @@ func Logout(c echo.Context) error {
 		SameSite: http.SameSiteLaxMode,
 	}
 	c.SetCookie(cookie)
+
+	refreshCookie := &http.Cookie{
+		Name:     "refresh_token",
+		Value:    "",
+		HttpOnly: true,
+		Secure:   c.Scheme() == "https",
+		Expires:  time.Unix(0, 0),
+		Path:     "/",
+		SameSite: http.SameSiteLaxMode,
+	}
+	c.SetCookie(refreshCookie)
+
 	return c.JSON(http.StatusOK, map[string]string{"message": "successfully logged out"})
 }
