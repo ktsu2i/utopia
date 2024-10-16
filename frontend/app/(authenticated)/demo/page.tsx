9a2caa71206224b11f71d6805637d96dd62af6cd
Kaito Tsutsui <101069375+ktsu2i@users.noreply.github.com>
2024-10-16 13:07:31 +0900

    Add loading spinner (#19)

--- frontend/app/(authenticated)/demo/page.tsx
+++ frontend/app/(authenticated)/demo/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { TailSpin } from "react-loader-spinner";
+
 import { Button } from "@/components/ui/button";
 import useAuth from "@/hooks/useAuth";
 
@@ -7,15 +9,19 @@ export default function Demo() {
   const { isAuthenticated, logout } = useAuth();
 
   if (isAuthenticated === null) {
-    return <div>Loading...</div>
+    return (
+      <div className="h-full flex items-center justify-center">
+        <TailSpin color="#FF9933" />
+      </div>
+    );
   }
 
   if (!isAuthenticated) return null;
 
   return (
-    <>
-      <h1>Demo page</h1>
+    <div className="h-full flex flex-col items-center justify-center">
+      <h1 className="text-2xl font-bold mb-5">Demo page</h1>
       <Button onClick={logout}>Logout</Button>
-    </>
+    </div>
   )
 }
