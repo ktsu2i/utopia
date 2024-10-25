63aac5b2680bf9a1432f7b0a8c4957d12f118a39
Kaito Tsutsui <kaito.tsu2i@gmail.com>
2024-10-25 12:54:22 +0900

    created new migration file for users

--- backend/migrations/20241025125345-users.sql
+++ backend/migrations/20241025125345-users.sql
@@ -5,8 +5,10 @@ CREATE TABLE IF NOT EXISTS `users` (
     `username` VARCHAR(255) NOT NULL UNIQUE,
     `first_name` VARCHAR(255) NOT NULL DEFAULT '',
     `last_name` VARCHAR(255) NOT NULL DEFAULT '',
-    `email` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL UNIQUE,
     `hashed_password` VARCHAR(255) NOT NULL,
+    `profile_image_url` VARCHAR(255) NOT NULL DEFAULT '',
+    `bio` TEXT NULL,
     `created_at` DATETIME(6) DEFAULT NULL,
     `updated_at` DATETIME(6) DEFAULT NULL,
     PRIMARY KEY (`id`)
