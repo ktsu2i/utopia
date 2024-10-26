747f36cc47d85e37843ef7946f031a7498c23d10
Kaito Tsutsui <kaito.tsu2i@gmail.com>
2024-10-27 00:24:05 +0900

    added post model

---
+++ backend/models/post.go
@@ -0,0 +1,12 @@
+package models
+
+import "time"
+
+// DB
+type Post struct {
+	ID        string    `json:"id"`
+	UserID    string    `json:"user_id"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
