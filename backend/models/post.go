aa19d3c82f4840b21621a35e6182d274efc971d6
Kaito Tsutsui <kaito.tsu2i@gmail.com>
2024-10-27 01:01:03 +0900

    added comments in Post

--- backend/models/post.go
+++ backend/models/post.go
@@ -7,6 +7,7 @@ type Post struct {
 	ID        string    `json:"id"`
 	UserID    string    `json:"user_id"`
 	Content   string    `json:"content"`
+	Comments  []Comment `json:"comments"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
