5b54b3cd8fbf10d62e66509ce0d1e9a765285c88
Kaito Tsutsui <kaito.tsu2i@gmail.com>
2024-10-28 13:11:04 +0900

    created GetPosts func

--- backend/handlers/post.go
+++ backend/handlers/post.go
@@ -1,11 +1,32 @@
 package handlers
 
 import (
+	"backend/db"
+	"backend/models"
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
 )
 
-func GetAllPosts(c echo.Context) error {
-	return c.JSON(http.StatusOK, nil)
+func GetPosts(c echo.Context) error {
+	// Default settings
+	page, err := strconv.Atoi(c.QueryParam("page"))
+	if err != nil || page < 1 {
+		page = 1
+	}
+
+	limit, err := strconv.Atoi(c.QueryParam("limit"))
+	if err != nil || limit < 1 || limit > 100 {
+		limit = 10
+	}
+
+	offset := (page - 1) * limit
+
+	var posts []models.Post
+	if err := db.DB.Preload("User").Order("created_at desc").Limit(limit).Offset(offset).Find(&posts).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
+	}
+
+	return c.JSON(http.StatusOK, posts)
 }
