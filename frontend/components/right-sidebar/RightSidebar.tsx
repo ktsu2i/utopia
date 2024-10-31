6b9c5007fdd852998643e459e127c97cbd697409
Kaito Tsutsui <kaito.tsu2i@gmail.com>
2024-11-01 01:38:53 +0900

    fixed some stuf

---
+++ frontend/components/right-sidebar/RightSidebar.tsx
@@ -0,0 +1,11 @@
+const RightSidebar = () => {
+  return (
+    <div className="h-full w-1/3 fixed mt-10">
+      <div className="">
+        Right Sidebar
+      </div>
+    </div>
+  )
+};
+
+export default RightSidebar;
--- frontend/components/sidebar/Sidebar.tsx
+++ frontend/components/sidebar/Sidebar.tsx
@@ -5,9 +5,7 @@ import SidebarRoutes from "./SidebarRoutes";
 const Sidebar = () => {
   return (
     <div className="h-full w-1/3 fixed mt-10">
-      <div className="">
-        <SidebarRoutes />
-      </div>
+      <SidebarRoutes />
     </div>
   )
 };
