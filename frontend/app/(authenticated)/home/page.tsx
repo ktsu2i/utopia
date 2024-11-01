03d3c24de1a9b1b0164652cc65f64441d7e1a8fc
Kaito Tsutsui <kaito.tsu2i@gmail.com>
2024-11-02 01:36:38 +0900

    set up for useSWRInfinite

--- frontend/app/(authenticated)/home/page.tsx
+++ frontend/app/(authenticated)/home/page.tsx
@@ -1,4 +1,30 @@
+import axios from "axios";
+import { useCallback } from "react";
+import useSWRInfinite from "swr/infinite";
+
+import { Post } from "@/lib/types";
+
 export default function Home() {
+  const getKey = (pageIndex: number, previousPageData: Post[][]) => {
+    if (previousPageData && !previousPageData.length) return null; // reaches the end
+    return `/posts?page=${pageIndex + 1}&limit=10`;
+  }
+
+  const fetcher = useCallback(
+    async (url: string) => await axios.get<Post[]>(url, { withCredentials: true }),
+    [],
+  );
+
+  const { data, size, setSize } = useSWRInfinite(
+    getKey, 
+    fetcher, 
+    {
+      revalidateIfStale: false,
+      revalidateOnFocus: false,
+      revalidateFirstPage: false,
+    }
+  );
+
   return (
     <div>Home</div>
   );
