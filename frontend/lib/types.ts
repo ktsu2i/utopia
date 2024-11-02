acf0e55ad6a2d9cf233e5e877cbffa89eb88c3fc
Kaito Tsutsui <101069375+ktsu2i@users.noreply.github.com>
2024-11-02 23:17:05 +0900

    Change Post type in frontend (#49)

--- frontend/lib/types.ts
+++ frontend/lib/types.ts
@@ -1,3 +1,15 @@
+export interface User {
+  id: string
+  firstName: string
+  lastName: string
+  username: string
+  email: string
+  profileImageUrl: string
+  bio: string
+  createdAt: string
+  updatedAt: string
+}
+
 export interface Reply {
   id: string
   userId: string
@@ -11,8 +23,8 @@ export interface Reply {
 export interface Post {
   id: string
   userId: string
+  user: User
   content: string
-  replies: Reply[]
   createdAt: string
   updatedAt: string
 }
