79023249abc75b5e9cc5d53fc56d65c03c7ae446
Kaito Tsutsui <kaito.tsu2i@gmail.com>
2024-11-02 23:53:46 +0900

    new design for post item

--- frontend/components/PostItem.tsx
+++ frontend/components/PostItem.tsx
@@ -1,4 +1,6 @@
 import { Post } from "@/lib/types";
+import { Avatar, AvatarFallback } from "./ui/avatar";
+import { Ellipsis } from "lucide-react";
 
 interface PostItemProps {
   key: number
@@ -11,7 +13,22 @@ const PostItem: React.FC<PostItemProps> = ({
 }) => {
   return (
     <div key={key} className="border-b border-x border-gray-300 last:border-b-0 p-4">
-      {post.content}
+      <div className="flex gap-x-2">
+        <div className="h-full">
+          <Avatar>
+            <AvatarFallback>
+              {post.user.username.substring(0, 1).toUpperCase()}
+            </AvatarFallback>
+          </Avatar>
+        </div>
+        <div className="w-full">
+          <div className="flex justify-between">
+            <span className="font-semibold">{post.user.username}</span>
+            <Ellipsis className="h-4 w-4" color="gray" />
+          </div>
+          <div>{post.content}</div>
+        </div>
+      </div>
     </div>
   );
 }
