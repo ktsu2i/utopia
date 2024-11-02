1669f1a57ea860ec5f773944205558b0edb494a3
Kaito Tsutsui <101069375+ktsu2i@users.noreply.github.com>
2024-11-03 02:59:46 +0900

    Update UI of post item (#50)

--- frontend/components/PostItem.tsx
+++ frontend/components/PostItem.tsx
@@ -1,4 +1,11 @@
+"use client";
+
 import { Post } from "@/lib/types";
+import { Avatar, AvatarFallback } from "./ui/avatar";
+import { Ellipsis } from "lucide-react";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import { Button } from "./ui/button";
+import useCurrentUser from "@/hooks/useCurrentUser";
 
 interface PostItemProps {
   key: number
@@ -9,9 +16,47 @@ const PostItem: React.FC<PostItemProps> = ({
   key,
   post,
 }) => {
+  const { currentUser } = useCurrentUser();
+  const isMe = currentUser?.id === post.userId;
+
   return (
     <div key={key} className="border-b border-x border-gray-300 last:border-b-0 p-4">
-      {post.content}
+      <div className="flex gap-x-2">
+        <div className="h-full">
+          <Avatar>
+            <AvatarFallback>
+              {post.user.username.substring(0, 1).toUpperCase()}
+            </AvatarFallback>
+          </Avatar>
+        </div>
+        <div className="w-full">
+          <div className="flex justify-between">
+            <span className="font-semibold">{post.user.username}</span>
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button variant="ghost" size="sm">
+                  <Ellipsis className="h-4 w-4" color="gray" />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent className={`flex flex-col p-2 ${isMe ? "w-24" : "w-32"}`}>
+                {isMe ? (
+                  <>
+                    <Button variant="ghost" className="justify-start">Edit</Button>
+                    <Button variant="ghost" className="justify-start text-red-600 hover:text-red-600">Delete</Button>
+                  </>
+                ) : (
+                  <>
+                    <Button variant="ghost" className="justify-start">Follow {post.user.username}</Button>
+                    <Button variant="ghost" className="justify-start">Mute {post.user.username}</Button>
+                    <Button variant="ghost" className="justify-start text-red-600 hover:text-red-600">Block {post.user.username}</Button>
+                  </>
+                )}
+              </PopoverContent>
+            </Popover>
+          </div>
+          <div>{post.content}</div>
+        </div>
+      </div>
     </div>
   );
 }
