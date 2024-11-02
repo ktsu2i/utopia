a6133ca58630e08067a831492ab85b1d34d78eb6
Kaito Tsutsui <kaito.tsu2i@gmail.com>
2024-11-03 04:18:02 +0900

    created post card component

---
+++ frontend/components/right-sidebar/PostCard.tsx
@@ -0,0 +1,16 @@
+import { Card, CardContent, CardHeader } from "../ui/card";
+
+const PostCard = () => {
+  return (
+    <div className="m-4">
+      <Card>
+        <CardHeader>Express yourself in Utopia!</CardHeader>
+        <CardContent>
+          post
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
+
+export default PostCard;
