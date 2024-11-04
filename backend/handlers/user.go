9fa4318e034a8f68f5be626758debbac2bd2c4c9
Kaito Tsutsui <kaito.tsu2i@gmail.com>
2024-11-04 21:47:05 +0900

    created UpdateUser

--- backend/handlers/user.go
+++ backend/handlers/user.go
@@ -125,6 +125,32 @@ func GetUserById(c echo.Context) error {
 	return c.JSON(http.StatusOK, res)
 }
 
+func UpdateUser(c echo.Context) error {
+	id := c.Param("id")
+
+	var req models.UserUpdateParams
+	if err := c.Bind(&req); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"message": err.Error()})
+	}
+
+	if err := db.DB.Where("id = ?", id).First(&models.User{}).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message": "User not found"})
+	}
+
+	u := models.User{
+		AccountName: req.AccountName,
+		Username:    req.Username,
+		FirstName:   req.FirstName,
+		LastName:    req.LastName,
+	}
+
+	if err := db.DB.Save(&u).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
+	}
+
+	return c.JSON(http.StatusOK, map[string]string{"message": "User successfully updated"})
+}
+
 func DeleteUserById(c echo.Context) error {
 	id := c.Param("id")
 	if db.DB.Where("id = ?", id).Delete(&models.User{}).RowsAffected == 0 {
