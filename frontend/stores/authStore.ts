05d25f19ec3844bbb5ab3b272b96949972537fd3
Kaito Tsutsui <kaito.tsu2i@gmail.com>
2024-11-04 15:00:15 +0900

    removed login and logout in authStore

--- frontend/stores/authStore.ts
+++ frontend/stores/authStore.ts
@@ -1,18 +1,13 @@
-import { User } from "@/lib/types";
 import { create } from "zustand";
 
 interface AuthState {
   isAuthenticated: boolean;
-  currentUser: User | null;
-  login: (user: User) => void;
-  logout: () => void;
+  setIsAuthenticated: (auth: boolean) => void;
 }
 
 const useAuthStore = create<AuthState>((set) => ({
   isAuthenticated: false,
-  currentUser: null,
-  login: (user) => set({ isAuthenticated: true, currentUser: user }),
-  logout: () => set({ isAuthenticated: false, currentUser: null }),
+  setIsAuthenticated: (auth) => set({ isAuthenticated: auth }),
 }));
 
 export default useAuthStore;
