f17e3efe5aa1b640306644cce857d60addef2b39
Kaito Tsutsui <kaito.tsu2i@gmail.com>
2024-11-06 00:34:21 +0900

    completed websocket functions

--- backend/handlers/websocket.go
+++ backend/handlers/websocket.go
@@ -1,1 +1,63 @@
 package handlers
+
+import (
+	"net/http"
+	"sync"
+
+	"github.com/gorilla/websocket"
+	"github.com/labstack/echo/v4"
+)
+
+type WebSocketServer struct {
+	clients map[*websocket.Conn]bool
+	mu      sync.Mutex
+}
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+var wsServer = WebSocketServer{
+	clients: make(map[*websocket.Conn]bool),
+}
+
+func HandleWebSocket(c echo.Context) error {
+	conn, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	defer conn.Close()
+
+	// Add client
+	wsServer.mu.Lock()
+	wsServer.clients[conn] = true
+	wsServer.mu.Unlock()
+
+	defer func() {
+		wsServer.mu.Lock()
+		delete(wsServer.clients, conn)
+		wsServer.mu.Unlock()
+	}()
+
+	for {
+		_, _, err := conn.ReadMessage()
+		if err != nil {
+			break
+		}
+	}
+	return c.JSON(http.StatusOK, nil)
+}
+
+func NotifyClients(message string) {
+	wsServer.mu.Lock()
+	defer wsServer.mu.Unlock()
+	for client := range wsServer.clients {
+		err := client.WriteMessage(websocket.TextMessage, []byte(message))
+		if err != nil {
+			client.Close()
+			delete(wsServer.clients, client)
+		}
+	}
+}
