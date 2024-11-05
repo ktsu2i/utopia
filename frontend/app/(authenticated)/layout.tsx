b06e06f317428d38dc45eb4508e5f7dfd6ccb4b6
Kaito Tsutsui <kaito.tsu2i@gmail.com>
2024-11-05 14:47:56 +0900

    all pages under authenticated require auth

--- frontend/app/(authenticated)/layout.tsx
+++ frontend/app/(authenticated)/layout.tsx
@@ -1,11 +1,32 @@
+"use client";
+
 import RightSidebar from "@/components/right-sidebar/RightSidebar"
 import Sidebar from "@/components/sidebar/Sidebar"
+import useAuthStore from "@/stores/authStore"
+import { useRouter } from "next/navigation";
+import { TailSpin } from "react-loader-spinner";
 
 export default function Layout({
   children
 }: {
   children: React.ReactNode
 }) {
+  const router = useRouter();
+  const { isAuthenticated } = useAuthStore();
+
+  if (isAuthenticated === null) {
+    return (
+      <div className="h-full flex items-center justify-center">
+        <TailSpin color="#FF9933" />
+      </div>
+    );
+  }
+
+  if (!isAuthenticated) {
+    router.push("/login");
+    return null;
+  };
+
   return (
     <>
       <Sidebar />
