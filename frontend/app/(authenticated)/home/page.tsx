4d884da9cf2ddcc2793d50ecb8c83abd8412f544
Kaito Tsutsui <101069375+ktsu2i@users.noreply.github.com>
2024-11-06 01:10:14 +0900

    Notify when deleting user (#73)

--- frontend/app/(authenticated)/home/page.tsx
+++ frontend/app/(authenticated)/home/page.tsx
@@ -50,7 +50,7 @@ export default function Home() {
     const socket = new WebSocket("ws://localhost:8080/api/ws");
     
     socket.onmessage = (event) => {
-      if (event.data === "create_post" || event.data === "delete_post") {
+      if (event.data === "create_post" || event.data === "delete_post" || event.data === "delete_user") {
         mutate();
       }
     };
