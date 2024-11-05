dc8bb9a0eb1a55d37263829db8e5847db843bdd6
Kaito Tsutsui <kaito.tsu2i@gmail.com>
2024-11-05 14:58:05 +0900

    hide sidebars while loading

--- frontend/components/sidebar/Sidebar.tsx
+++ frontend/components/sidebar/Sidebar.tsx
@@ -1,8 +1,11 @@
+import useAuthStore from "@/stores/authStore";
 import SidebarRoutes from "./SidebarRoutes";
 
 const Sidebar = () => {
+  const { isAuthenticated } = useAuthStore();
+
   return (
-    <div className="h-full w-1/3 fixed">
+    <div className={`h-full w-1/3 fixed ${!isAuthenticated && "hidden"}`}>
       <SidebarRoutes />
     </div>
   )
