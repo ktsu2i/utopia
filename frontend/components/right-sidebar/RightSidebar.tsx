dc2cf6d1d34129a6c6d18a099d8328c43ad16c23
Kaito Tsutsui <kaito.tsu2i@gmail.com>
2024-11-05 23:16:10 +0900

    added use client

--- frontend/components/right-sidebar/RightSidebar.tsx
+++ frontend/components/right-sidebar/RightSidebar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import useAuthStore from "@/stores/authStore";
 import PostCard from "./PostCard";
 
--- frontend/components/sidebar/Sidebar.tsx
+++ frontend/components/sidebar/Sidebar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import useAuthStore from "@/stores/authStore";
 import SidebarRoutes from "./SidebarRoutes";
 
