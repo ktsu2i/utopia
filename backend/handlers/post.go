47efa18cd2f9d834884e0ec4006797a592810519
Kaito Tsutsui <kaito.tsu2i@gmail.com>
2024-11-06 00:42:38 +0900

    changed notify message

--- backend/handlers/post.go
+++ backend/handlers/post.go
@@ -31,7 +31,7 @@ func CreatePost(c echo.Context) error {
 		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
 	}
 
-	NotifyClients("New post created")
+	NotifyClients("new_post")
 
 	return c.JSON(http.StatusOK, p)
 }
