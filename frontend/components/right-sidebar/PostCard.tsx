c796d45bd3bab907ad69d031ca5cfe7825983116
Kaito Tsutsui <kaito.tsu2i@gmail.com>
2024-11-07 14:11:02 +0900

    added optional ?

--- frontend/components/right-sidebar/PostCard.tsx
+++ frontend/components/right-sidebar/PostCard.tsx
@@ -73,7 +73,7 @@ const PostCard = () => {
             <div className="h-full">
               <Avatar>
                 <AvatarFallback>
-                  {currentUser?.accountName.substring(0, 1).toUpperCase()}
+                  {currentUser?.accountName?.substring(0, 1).toUpperCase()}
                 </AvatarFallback>
               </Avatar>
             </div>
