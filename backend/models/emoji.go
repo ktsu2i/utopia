b5c720b0334799b5d4ddaa223bd21961294c3259
Kaito Tsutsui <kaito.tsu2i@gmail.com>
2024-11-08 00:23:27 +0900

    updated type struct

--- backend/models/emoji.go
+++ backend/models/emoji.go
@@ -3,11 +3,11 @@ package models
 import "time"
 
 type Emoji struct {
-	ID        int16     `json:"id"`
+	ID        int       `json:"id"`
 	Name      string    `json:"name"`
 	Unicode   string    `json:"unicode"`
 	ImageURL  string    `json:"imageUrl"`
-	CreatorID string    `json:"creatorId"`
+	CreatorID *int      `json:"creatorId"`
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
 }
