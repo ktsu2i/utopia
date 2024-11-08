f3cff29f636c870d4321d3131cee8a2a5da17e13
Kaito Tsutsui <kaito.tsu2i@gmail.com>
2024-11-08 16:26:06 +0900

    created reaction handler

---
+++ backend/handlers/reaction.go
@@ -0,0 +1,41 @@
+package handlers
+
+import (
+	"backend/db"
+	"backend/models"
+	"net/http"
+	"time"
+
+	"github.com/labstack/echo/v4"
+)
+
+func AddReaction(c echo.Context) error {
+	postID := c.Param("post_id")
+	emojiID := c.Param("emoji_id")
+
+	userID, err := GetUserID(c)
+	if err != nil {
+		return c.JSON(http.StatusUnauthorized, map[string]string{"message": "Unauthorized"})
+	}
+
+	var req models.ReactionParams
+	if err := c.Bind(&req); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"message": err.Error()})
+	}
+
+	r := models.Reaction{
+		UserID:    userID,
+		PostID:    postID,
+		EmojiID:   emojiID,
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+	}
+
+	if err := db.DB.Create(&r).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
+	}
+
+	NotifyClients("add_emoji")
+
+	return c.JSON(http.StatusOK, r)
+}
--- backend/models/reaction.go
+++ backend/models/reaction.go
@@ -1,7 +1,17 @@
 package models
 
+import "time"
+
 type ReactionParams struct {
 	PostID  string `json:"postId"`
 	UserID  string `json:"userId"`
 	EmojiID string `json:"emojiId"`
+}
+
+type Reaction struct {
+	PostID    string    `json:"postId"`
+	UserID    string    `json:"userId"`
+	EmojiID   string    `json:"emojiId"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
 }
