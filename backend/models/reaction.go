4a6d5e387ddf991c88edf1fa60b59ee179cca6ac
Kaito Tsutsui <kaito.tsu2i@gmail.com>
2024-11-09 01:28:31 +0900

    changed emojiID type to int

--- backend/models/reaction.go
+++ backend/models/reaction.go
@@ -4,13 +4,13 @@ import "time"
 
 type ReactionParams struct {
 	PostID  string `json:"postId"`
-	EmojiID string `json:"emojiId"`
+	EmojiID int    `json:"emojiId"`
 }
 
 type Reaction struct {
 	PostID    string    `json:"postId"`
 	UserID    string    `json:"userId"`
-	EmojiID   string    `json:"emojiId"`
+	EmojiID   int       `json:"emojiId"`
 	CreatedAt time.Time `json:"createdAt"`
 	UpdatedAt time.Time `json:"updatedAt"`
 }
