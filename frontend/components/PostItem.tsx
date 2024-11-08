474839245d77f061bfdd30a0be851364e5893841
Kaito Tsutsui <kaito.tsu2i@gmail.com>
2024-11-08 16:10:14 +0900

    added emoji popover

--- frontend/components/PostItem.tsx
+++ frontend/components/PostItem.tsx
@@ -2,7 +2,7 @@
 
 import { Post } from "@/lib/types";
 import { Avatar, AvatarFallback } from "./ui/avatar";
-import { Ellipsis } from "lucide-react";
+import { Ellipsis, SmilePlus } from "lucide-react";
 import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 import { Button, buttonVariants } from "./ui/button";
 import useAuthStore from "@/stores/authStore";
@@ -11,6 +11,7 @@ import axios from "axios";
 import toast from "react-hot-toast";
 import { useState } from "react";
 import { formatDistanceToNowStrict, parseISO } from "date-fns";
+import { useEmojis } from "@/hooks/useEmojis";
 
 interface PostItemProps {
   post: Post
@@ -21,6 +22,7 @@ const PostItem: React.FC<PostItemProps> = ({
 }) => {
   const [isOpen, setIsOpen] = useState(false);
   const { currentUser } = useAuthStore();
+  const { emojis } = useEmojis();
 
   const isMe = currentUser?.id === post.userId;
   const postedDate = formatDistanceToNowStrict(parseISO(post.updatedAt));
@@ -37,69 +39,96 @@ const PostItem: React.FC<PostItemProps> = ({
 
   return (
     <div className="border-b border-gray-300 last:border-b-0 p-4">
-      <div className="flex gap-x-2">
-        <div className="h-full">
-          <Avatar>
-            <AvatarFallback>
-              {post.user.accountName.substring(0, 1).toUpperCase()}
-            </AvatarFallback>
-          </Avatar>
-        </div>
-        <div className="w-full">
-          <div className="flex justify-between">
-            <div className="flex flex-col pb-2">
-              <span className="font-semibold">{post.user.accountName}</span>
-              <span className="text-sm text-gray-500">{"@" + post.user.username} &middot; {postedDate}</span>
-            </div>
-            <Popover open={isOpen} onOpenChange={setIsOpen}>
-              <PopoverTrigger asChild>
-                <Button variant="ghost" size="sm">
-                  <Ellipsis className="h-4 w-4" color="gray" />
-                </Button>
-              </PopoverTrigger>
-              <PopoverContent className="flex flex-col p-2 w-24">
-                {isMe ? (
-                  <>
-                    <Button variant="ghost" className="justify-start">Edit</Button>
-                    <AlertDialog>
-                      <AlertDialogTrigger asChild>
-                        <Button
-                          variant="ghost"
-                          className="justify-start text-red-600 hover:text-red-600"
-                        >
-                          Delete
-                        </Button>
-                      </AlertDialogTrigger>
-                      <AlertDialogContent>
-                        <AlertDialogHeader>
-                          <AlertDialogTitle>Are you sure?</AlertDialogTitle>
-                          <AlertDialogDescription>
-                            This will permanently delete this post. 
-                          </AlertDialogDescription>
-                        </AlertDialogHeader>
-                        <AlertDialogFooter>
-                          <AlertDialogCancel>Cancel</AlertDialogCancel>
-                          <AlertDialogAction
-                            onClick={onClick} 
-                            className={buttonVariants({ variant: "destructive" })}
+      <div className="flex flex-col gap-y-2">
+        <div className="flex gap-x-2">
+          <div className="h-full">
+            <Avatar>
+              <AvatarFallback>
+                {post.user.accountName.substring(0, 1).toUpperCase()}
+              </AvatarFallback>
+            </Avatar>
+          </div>
+          <div className="w-full">
+            <div className="flex justify-between">
+              <div className="flex flex-col pb-2">
+                <span className="font-semibold">{post.user.accountName}</span>
+                <span className="text-sm text-gray-500">{"@" + post.user.username} &middot; {postedDate}</span>
+              </div>
+              <Popover open={isOpen} onOpenChange={setIsOpen}>
+                <PopoverTrigger asChild>
+                  <Button variant="ghost" size="sm">
+                    <Ellipsis className="h-4 w-4" color="gray" />
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className="flex flex-col p-2 w-24">
+                  {isMe ? (
+                    <>
+                      <Button variant="ghost" className="justify-start">Edit</Button>
+                      <AlertDialog>
+                        <AlertDialogTrigger asChild>
+                          <Button
+                            variant="ghost"
+                            className="justify-start text-red-600 hover:text-red-600"
                           >
                             Delete
-                          </AlertDialogAction>
-                        </AlertDialogFooter>
-                      </AlertDialogContent>
-                    </AlertDialog>
-                  </>
-                ) : (
-                  <>
-                    <Button variant="ghost" className="justify-start">Follow</Button>
-                    <Button variant="ghost" className="justify-start">Mute</Button>
-                    <Button variant="ghost" className="justify-start text-red-600 hover:text-red-600">Block</Button>
-                  </>
-                )}
-              </PopoverContent>
-            </Popover>
+                          </Button>
+                        </AlertDialogTrigger>
+                        <AlertDialogContent>
+                          <AlertDialogHeader>
+                            <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+                            <AlertDialogDescription>
+                              This will permanently delete this post. 
+                            </AlertDialogDescription>
+                          </AlertDialogHeader>
+                          <AlertDialogFooter>
+                            <AlertDialogCancel>Cancel</AlertDialogCancel>
+                            <AlertDialogAction
+                              onClick={onClick} 
+                              className={buttonVariants({ variant: "destructive" })}
+                            >
+                              Delete
+                            </AlertDialogAction>
+                          </AlertDialogFooter>
+                        </AlertDialogContent>
+                      </AlertDialog>
+                    </>
+                  ) : (
+                    <>
+                      <Button variant="ghost" className="justify-start">Follow</Button>
+                      <Button variant="ghost" className="justify-start">Mute</Button>
+                      <Button variant="ghost" className="justify-start text-red-600 hover:text-red-600">Block</Button>
+                    </>
+                  )}
+                </PopoverContent>
+              </Popover>
+            </div>
+            <div className="whitespace-pre-wrap">{post.content}</div>
           </div>
-          <div className="whitespace-pre-wrap">{post.content}</div>
+        </div>
+
+        {/* Footer */}
+        <div className="w-full">
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button variant="ghost" size="sm" className="text-gray-500">
+                <SmilePlus className="h-5 w-5" />
+                <span className="sr-only">Add reaction</span>
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent>
+              <div className="grid grid-cols-4 gap-2">
+                {emojis.map((emoji) => (
+                  <Button
+                    key={emoji.id}
+                    variant="ghost"
+                    className="text-2xl p-2"
+                  >
+                    {String.fromCodePoint(parseInt(emoji.unicode, 16))}
+                  </Button>
+                ))}
+              </div>
+            </PopoverContent>
+          </Popover>
         </div>
       </div>
     </div>
