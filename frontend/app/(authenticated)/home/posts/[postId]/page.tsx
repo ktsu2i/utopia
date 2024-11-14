897a4eadc130c14558cdb356610e44078f3e88e3
ktsu2i <kaito.tsu2i@gmail.com>
2024-11-14 15:36:44 +0900

    completed reply ui

--- frontend/app/(authenticated)/home/posts/[postId]/page.tsx
+++ frontend/app/(authenticated)/home/posts/[postId]/page.tsx
@@ -1,14 +1,32 @@
 "use client";
 
 import PostItem from "@/components/PostItem";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
+import { Textarea } from "@/components/ui/textarea";
 import { Post } from "@/lib/types";
+import useAuthStore from "@/stores/authStore";
+import { zodResolver } from "@hookform/resolvers/zod";
 import axios from "axios";
+import { Send } from "lucide-react";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 import { TailSpin } from "react-loader-spinner";
+import { z } from "zod";
+
+const ReplySchema = z.object({
+  content: z
+    .string()
+    .trim()
+    .min(1, { message: "Reply must be at least 1 character." })
+    .max(150, { message: "Reply must be less than 151 characters." }),
+});
 
 export default function PostDetails() {
   const { postId } = useParams();
+  const { currentUser } = useAuthStore();
   const [post, setPost] = useState<Post | null>(null);
 
   useEffect(() => {
@@ -38,6 +56,17 @@ export default function PostDetails() {
     };
   }, [postId]);
 
+  const form = useForm<z.infer<typeof ReplySchema>>({
+    resolver: zodResolver(ReplySchema),
+    defaultValues: {
+      content: "",
+    },
+  });
+
+  const onSubmit = () => {
+    // todo
+  };
+
   if (!post) {
     return (
       <div className="h-full flex items-center justify-center">
@@ -48,8 +77,45 @@ export default function PostDetails() {
 
   return (
     <>
-      <div className="flex flex-col justify-center">
+      <div className="flex flex-col justify-center border-b border-gray-300">
         <PostItem post={post} isSelected={true} />
+        <div className=" flex gap-x-2 p-4">
+          <Avatar>
+            <AvatarFallback>
+              {currentUser?.accountName?.substring(0, 1).toUpperCase()}
+            </AvatarFallback>
+          </Avatar>
+          <div className="flex items-center w-full gap-x-2">
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
+                <FormField
+                  control={form.control}
+                  name="content"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Textarea
+                          placeholder="Encourage others!"
+                          className="w-full resize-none"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </form>
+            </Form>
+            <Button
+              variant="ghost"
+              size="sm"
+              className="rounded-full w-8 h-8 p-1 hover:text-utopia hover:bg-utopia_light"
+            >
+              <Send className="h-5 w-5" />
+              <span className="sr-only">Reply</span>
+            </Button>
+          </div>
+        </div>
       </div>
     </>
   );
