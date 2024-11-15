91c9fe7cb283ff3cfc116a5062b19bfdfd4d4772
ktsu2i <kaito.tsu2i@gmail.com>
2024-11-16 00:04:11 +0900

    created reply item

---
+++ frontend/components/ReplyItem.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { Reply } from "@/lib/types";
+
+interface ReplyItemProps {
+	reply: Reply;
+}
+
+const ReplyItem: React.FC<ReplyItemProps> = ({
+	reply,
+}) => {
+	return (
+		<div>aa</div>
+	);
+};
+
+export default ReplyItem;
