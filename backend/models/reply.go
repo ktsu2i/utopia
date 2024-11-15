827b9a5bce07fae5207d378065e1cc0e8ed41dfc
ktsu2i <kaito.tsu2i@gmail.com>
2024-11-15 11:14:57 +0900

    added reply model

--- backend/models/reply.go
+++ backend/models/reply.go
@@ -2,13 +2,21 @@ package models
 
 import "time"
 
+// Request
+type ReplyParams struct {
+	Post    PostResult `json:"post"`
+	Content string     `json:"content"`
+}
+
 // DB
 type Reply struct {
-	ID            string    `json:"id"`
-	UserID        string    `json:"userId"`
-	PostID        string    `json:"postId"`
-	ParentReplyID string    `json:"parentReplyRd"`
-	Content       string    `json:"content"`
-	CreatedAt     time.Time `json:"createdAt"`
-	UpdatedAt     time.Time `json:"updatedAt"`
+	ID            string     `json:"id"`
+	UserID        string     `json:"userId"`
+	User          UserResult `gorm:"foreignKey:UserID;references:ID" json:"user"`
+	PostID        string     `json:"postId"`
+	Post          PostResult `gorm:"foreignKey:PostID;references:ID" json:"post"`
+	ParentReplyID string     `json:"parentReplyRd"`
+	Content       string     `json:"content"`
+	CreatedAt     time.Time  `json:"createdAt"`
+	UpdatedAt     time.Time  `json:"updatedAt"`
 }
