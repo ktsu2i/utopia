56805e35c46488b89fd20ea145600ccef0fde235
ktsu2i <kaito.tsu2i@gmail.com>
2024-11-18 23:36:36 +0900

    created get followers api

--- backend/handlers/follower.go
+++ backend/handlers/follower.go
@@ -4,6 +4,7 @@ import (
 	"backend/db"
 	"backend/models"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/labstack/echo/v4"
@@ -48,4 +49,38 @@ func Unfollow(c echo.Context) error {
 	NotifyClients("unfollow")
 
 	return c.JSON(http.StatusOK, nil)
+}
+
+func GetFollowers(c echo.Context) error {
+	userID, err := GetUserID(c)
+	if err != nil {
+		return c.JSON(http.StatusUnauthorized, map[string]string{"message": "Unauthorized"})
+	}
+
+	// Default settings
+	page, err := strconv.Atoi(c.QueryParam("page"))
+	if err != nil || page < 1 {
+		page = 1
+	}
+
+	limit, err := strconv.Atoi(c.QueryParam("limit"))
+	if err != nil || limit < 1 || limit > 100 {
+		limit = 10
+	}
+
+	offset := (page - 1) * limit
+
+	var followers []models.User
+	if err := db.DB.
+		Table("followers").
+		Select("users.*").
+		Joins("JOIN users ON users.id = followers.following_id").
+		Where("followers.followed_id = ?", userID).
+		Limit(limit).
+		Offset(offset).
+		Find(&followers).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"message": err.Error()})
+	}
+
+	return c.JSON(http.StatusOK, followers)
 }
--- backend/route.go
+++ backend/route.go
@@ -39,6 +39,7 @@ func route(e *echo.Echo) {
 	api.GET("/emojis", handlers.GetEmojis)
 	api.POST("/reactions", handlers.AddReaction)
 	api.DELETE("/reactions/:id", handlers.DeleteReaction)
+	api.GET("/followers", handlers.GetFollowers)
 	api.POST("/followers/:followedId", handlers.Follow)
 	api.DELETE("/followers/:followedId", handlers.Unfollow)
 	api.POST("/messages", handlers.CreateMessage)
