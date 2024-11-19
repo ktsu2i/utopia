1fda262cb8ae3d1d9c4392ca71f6d2371e43b4d1
ktsu2i <kaito.tsu2i@gmail.com>
2024-11-19 15:58:05 +0900

    updated user item and messages page

--- frontend/app/(authenticated)/messages/_components/UserItem.tsx
+++ frontend/app/(authenticated)/messages/_components/UserItem.tsx
@@ -1,6 +1,24 @@
-const UserItem = () => {
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { User } from "@/lib/types";
+
+interface UserItemProps {
+  user: User;
+}
+
+const UserItem: React.FC<UserItemProps> = ({
+  user,
+}) => {
   return (
-    <div>user</div>
+    <div>
+      <div className="flex gap-x-2">
+        <Avatar>
+          <AvatarFallback>
+            {user.accountName.substring(0, 1).toUpperCase()}
+          </AvatarFallback>
+        </Avatar>
+        <div>{user.accountName}</div>
+      </div>
+    </div>
   );
 };
 
--- frontend/app/(authenticated)/messages/page.tsx
+++ frontend/app/(authenticated)/messages/page.tsx
@@ -1,14 +1,62 @@
 "use client";
 
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Textarea } from "@/components/ui/textarea";
 import useAuthStore from "@/stores/authStore";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const MessageSchema = z.object({
+  content: z
+    .string()
+    .trim()
+    .min(1, { message: "Message must be at least 1 character." })
+    .max(150, { message: "Message must be less than 151 characters." }),
+});
 
 export default function Messages() {
   const { isAuthenticated } = useAuthStore();
 
+  const form = useForm<z.infer<typeof MessageSchema>>({
+    resolver: zodResolver(MessageSchema),
+    defaultValues: {
+      content: "",
+    },
+  });
+
+  const onSubmit = () => {
+    // todo
+  };
+
   return (
     <>
       {isAuthenticated && (
-        <div className="text-2xl font-bold border-b border-gray-300 p-4">Messages</div>
+        <div className="relative h-screen flex flex-col">
+          <div className="text-2xl font-bold border-b border-gray-300 p-4">Messages</div>
+          
+          {/* Input area */}
+          {/* <div className="sticky bottom-0 w-full bg-slate-300">
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)}>
+                <FormField
+                  control={form.control}
+                  name="content"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Textarea
+                          className="h-10 resize-none"
+                          {...field}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  )}
+                />
+              </form>
+            </Form>
+          </div> */}
+        </div>
       )}
     </>
   );
