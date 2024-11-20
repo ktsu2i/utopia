c3d7afdccff4a454b9f12760704ca8c9e482a18b
ktsu2i <kaito.tsu2i@gmail.com>
2024-11-21 00:53:03 +0900

    added header to user message page

--- frontend/app/(authenticated)/messages/[userId]/page.tsx
+++ frontend/app/(authenticated)/messages/[userId]/page.tsx
@@ -1,11 +1,35 @@
 "use client";
 
+import { User } from "@/lib/types";
+import useAuthStore from "@/stores/authStore";
+import axios from "axios";
 import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function ChatPage() {
   const { userId } = useParams();
+  const { isAuthenticated } = useAuthStore();
+  const [user, setUser] = useState<User | null>(null);
+
+  // fetch user
+  useEffect(() => {
+    const fetchUser = async () => {
+      const res = await axios.get<User>(`http://localhost:8080/api/users/${userId}`, { withCredentials: true });
+      setUser(res.data);
+    };
+
+    fetchUser();
+  }, [userId]);
   
   return (
-    <div>{userId}</div>
+    <>
+      {isAuthenticated && (
+        <div className="relative h-screen">
+          <div className="text-2xl font-bold border-b border-gray-300 p-6">
+            {user?.accountName}
+          </div>
+        </div>
+      )}
+    </>
   );
 }
