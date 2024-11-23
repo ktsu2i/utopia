6afc5fceb10032071ae2c75344e46716ef1065f7
ktsu2i <kaito.tsu2i@gmail.com>
2024-11-23 23:20:39 +0900

    added dm button

--- frontend/app/(authenticated)/search/_components/UserItem.tsx
+++ frontend/app/(authenticated)/search/_components/UserItem.tsx
@@ -1,7 +1,13 @@
+"use client";
+
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
 import { User } from "@/lib/types";
 import useAuthStore from "@/stores/authStore";
+import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 interface UserItemProps {
   user: User;
@@ -12,6 +18,7 @@ const UserItem: React.FC<UserItemProps> = ({
 }) => {
   const { currentUser } = useAuthStore();
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
 
   const onClick = () => {
     if (user.id === currentUser?.id) {
@@ -21,9 +28,13 @@ const UserItem: React.FC<UserItemProps> = ({
     }
   };
 
+  const navigateToDM = () => {
+    router.push(`/messages/${user.id}`);
+  };
+
   return (
-    <div className="w-full border-b border-gray-300 p-4">
-      <div onClick={onClick} className="flex items-center gap-x-4 cursor-pointer">
+    <div className="w-full flex items-center justify-between border-b border-gray-300 p-4">
+      <div onClick={onClick} className="w-full flex items-center gap-x-4 cursor-pointer">
         <Avatar>
           <AvatarFallback>
             {user.accountName.substring(0, 1).toUpperCase()}
@@ -33,6 +44,11 @@ const UserItem: React.FC<UserItemProps> = ({
           <div className="text-lg font-bold">{user.accountName}</div>
           <div className="text-gray-500">{"@" + user.username}</div>
         </div>
+      </div>
+      <div>
+        {currentUser?.id !== user.id && (
+          <Button variant="outline" size="sm" onClick={navigateToDM}>Message</Button>
+        )}
       </div>
     </div>
   );
