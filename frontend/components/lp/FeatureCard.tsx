278161d14613d1c12e989895f64dac48df6c9f17
ktsu2i <kaito.tsu2i@gmail.com>
2024-11-24 16:11:34 +0900

    updated feature card

--- frontend/components/lp/FeatureCard.tsx
+++ frontend/components/lp/FeatureCard.tsx
@@ -1,6 +1,24 @@
-const FeatureCard = () => {
+"use client";
+
+import { LucideIcon } from "lucide-react";
+
+interface FeatureCardProps {
+  icon: LucideIcon,
+  title: string,
+  description: string,
+}
+
+const FeatureCard: React.FC<FeatureCardProps> = ({
+  icon: Icon,
+  title,
+  description,
+}) => {
   return (
-    <div>Care</div>
+    <div>
+      <Icon className="h-8 w-8 text-utopia" />
+      <h3 className="mt-4 text-xl font-semibold text-gray-900">{title}</h3>
+      <p className="mt-2 text-gray-600">{description}</p>
+    </div>
   );
 };
 
