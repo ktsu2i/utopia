54e3d9a2181c7bf3eef138f9331bc974a7b2bc76
ktsu2i <kaito.tsu2i@gmail.com>
2024-11-24 14:44:41 +0900

    allow unlogged in users to visit lp

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,16 @@
+"use client";
+
+import useAuthStore from "@/stores/authStore";
+import { useRouter } from "next/navigation";
+
 export default function Root() {
+  const { isAuthenticated } = useAuthStore();
+  const router = useRouter();
+
+  if (isAuthenticated) {
+    router.push("/home");
+  }
+
   return (
     <div>LP</div>
   );
