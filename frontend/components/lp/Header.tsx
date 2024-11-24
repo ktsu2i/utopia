c72bbb174be4b4525a6ef731deed09e107eefb30
ktsu2i <kaito.tsu2i@gmail.com>
2024-11-24 17:04:15 +0900

    add bg-white to header

--- frontend/components/lp/Header.tsx
+++ frontend/components/lp/Header.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className="w-full flex justify-between border-b border-gray-300 px-10 py-4">
+    <div className="w-full flex justify-between bg-white border-b border-gray-300 px-10 py-4">
       <Image src="/images/logo.png" height={120} width={120} alt="Utopia" />
 
       <div className="flex gap-x-2">
