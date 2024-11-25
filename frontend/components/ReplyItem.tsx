39e4aba143466aaf197769c061c27fbb484f8dff
Sumana Reddy <sumanareddyarss@gmail.com>
2024-11-25 16:05:51 +0900

    Highlights the reply to reply's  button when the cursor is hovered over it

--- frontend/components/ReplyItem.tsx
+++ frontend/components/ReplyItem.tsx
@@ -265,7 +265,7 @@ const ReplyItem: React.FC<ReplyItemProps> = ({
 							</PopoverContent>
 						</Popover>
 					</div>
-					<div className="flex items-center gap-x-1 text-gray-500 pr-2">
+					<div onClick={onClick} className="flex items-center gap-x-1 text-gray-500 px-2 py-1 hover:text-utopia hover:bg-utopia_light rounded-2xl cursor-pointer">
 						<MessageCircle className="h-5 w-5" />
 						<span>{childReplyCount}</span>
 					</div>
