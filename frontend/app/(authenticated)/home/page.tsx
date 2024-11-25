a8116041d56c86ab2e1e3c6b1b28421315990077
ktsu2i <kaito.tsu2i@gmail.com>
2024-11-26 00:12:16 +0900

    implemented edit feature

--- frontend/app/(authenticated)/home/page.tsx
+++ frontend/app/(authenticated)/home/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
   const limit = 10;
   const isEmpty = data?.[0]?.length === 0;
   const isReachingEnd = isEmpty || (data && data?.[data?.length - 1]?.length < limit);
-  const eventTypes = useMemo(() => ["create_post", "delete_post", "delete_user", "add_reaction", "delete_reaction"], []);
+  const eventTypes = useMemo(() => ["create_post", "update_post", "delete_post", "delete_user", "add_reaction", "delete_reaction"], []);
 
   const { ref, inView: isScrollEnd } = useInView();
 
