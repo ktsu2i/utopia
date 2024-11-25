0d13cb5bced99e1fbdc7e96aec0c6043f4a2fdc2
Kaito Tsutsui <101069375+ktsu2i@users.noreply.github.com>
2024-11-25 10:34:56 +0900

    Display spinner when image loading (#127)

--- frontend/components/lp/Hero.tsx
+++ frontend/components/lp/Hero.tsx
@@ -4,9 +4,12 @@ import { ArrowRight } from "lucide-react";
 import { Button } from "../ui/button";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
+import { useState } from "react";
+import { TailSpin } from "react-loader-spinner";
 
 const Hero = () => {
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
 
   const onClick = () => {
     router.push("/sign-up");
@@ -26,7 +29,21 @@ const Hero = () => {
           Join a community where positivity thrives, everyone cares for one another, and every interaction makes the world a little brighter.
         </p>
 
-        <Image src="/images/mockup.svg" height={500} width={1000} alt="Mockup" />
+        <div className="relative flex justify-center items-center w-full h-[300px] md:h-[500px]">
+          {isLoading && (
+            <div className="absolute flex justify-center items-center w-full h-full">
+              <TailSpin color="#FF9933" />
+            </div>
+          )}
+          <Image
+            src="/images/mockup.svg"
+            height={500}
+            width={1000}
+            alt="Mockup"
+            className={`transition-opacity duration-500 ${isLoading ? "opacity-0" : "opacity-100"}`}
+            onLoadingComplete={() => setIsLoading(false)}
+          />
+        </div>
 
         <Button variant="utopia" size="lg" className="rounded-full mt-10" onClick={onClick}>
           Get Started
