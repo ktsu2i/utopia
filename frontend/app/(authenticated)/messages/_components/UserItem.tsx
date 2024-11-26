8112eac6e6a4303f20b42a9185e1939312c140f1
sumana0406 <108276108+sumana0406@users.noreply.github.com>
2024-11-27 00:25:30 +0900

    Shows the background bg-gray-50 when the cursor hovers over the users dm (#133)

--- frontend/app/(authenticated)/messages/_components/UserItem.tsx
+++ frontend/app/(authenticated)/messages/_components/UserItem.tsx
@@ -16,8 +16,8 @@ const UserItem: React.FC<UserItemProps> = ({
   };
 
   return (
-    <div className="w-full border-b border-gray-300 p-4">
-      <div onClick={onClick} className="flex items-center gap-x-4 cursor-pointer">
+    <div className="w-full border-b border-gray-300 p-4 hover:bg-gray-50 cursor-pointer">
+      <div onClick={onClick} className="flex items-center gap-x-4">
         <Avatar>
           <AvatarFallback>
             {user.accountName.substring(0, 1).toUpperCase()}
