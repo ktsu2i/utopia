f4a7bc57849d18ffd7f52c0f4f7570ea1a8857be
ktsu2i <kaito.tsu2i@gmail.com>
2024-11-28 00:40:24 +0900

    removed comment

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -8,7 +8,6 @@ export function cn(...inputs: ClassValue[]) {
 // Convert unicode to emoji
 export function parseEmoji(unicode: string) {
   // exception for red heart
-  console.log(unicode);
   if (unicode === "2764") {
     return String.fromCodePoint(parseInt(unicode, 16)) + String.fromCodePoint(0xFE0F);
   }
