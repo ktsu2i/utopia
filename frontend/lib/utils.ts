609911db8c13a9ce81eb9f6bc283fdc5ce495c3e
ktsu2i <kaito.tsu2i@gmail.com>
2024-11-28 00:39:41 +0900

    added exception

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -7,5 +7,10 @@ export function cn(...inputs: ClassValue[]) {
 
 // Convert unicode to emoji
 export function parseEmoji(unicode: string) {
+  // exception for red heart
+  console.log(unicode);
+  if (unicode === "2764") {
+    return String.fromCodePoint(parseInt(unicode, 16)) + String.fromCodePoint(0xFE0F);
+  }
   return String.fromCodePoint(parseInt(unicode, 16))
 }
