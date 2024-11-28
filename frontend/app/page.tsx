d532f38a30681226be0f7cec3a9fa157adf8767b
Kaito Tsutsui <101069375+ktsu2i@users.noreply.github.com>
2024-11-28 14:39:09 +0900

    Removed unnecessary code (#142)

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,17 +4,8 @@ import Features from "@/components/lp/Features";
 import Footer from "@/components/lp/Footer";
 import Header from "@/components/lp/Header";
 import Hero from "@/components/lp/Hero";
-import useAuthStore from "@/stores/authStore";
-import { useRouter } from "next/navigation";
 
 export default function Root() {
-  const { isAuthenticated } = useAuthStore();
-  const router = useRouter();
-
-  if (isAuthenticated) {
-    router.push("/home");
-  }
-
   return (
     <div className="relative">
       <div className="sticky top-0 z-10">
