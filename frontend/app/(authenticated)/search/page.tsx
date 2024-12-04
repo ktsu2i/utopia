59e817e3654324e622081ba523072e9f4cea0e68
ktsu2i <kaito.tsu2i@gmail.com>
2024-12-04 14:24:08 +0900

    removed from search/page.tsx

--- frontend/app/(authenticated)/search/page.tsx
+++ frontend/app/(authenticated)/search/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import PostItem from "@/components/PostItem";
-import { Button } from "@/components/ui/button";
 import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
