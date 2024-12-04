833b0fd079d455d2c4827082e4609920731a00fe
ktsu2i <kaito.tsu2i@gmail.com>
2024-12-04 14:33:19 +0900

    added use client

--- frontend/components/MobilePostButton.tsx
+++ frontend/components/MobilePostButton.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Plus } from "lucide-react";
 import { Button } from "./ui/button";
 import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "./ui/dialog";
